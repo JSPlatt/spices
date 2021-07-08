ded428ca8d11d87ac507b23894142c4109b99b8b
Jeffrey Platt <jsplatt@gmail.com>
2021-07-08 18:04:57 -0400

    day over

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,18 +7,19 @@ function Header() {
     return (
         <div className="header">
             <div className="header__left">
-                <img className="header__logo" src="https://i.pinimg.com/originals/d0/6c/0a/d06c0a9aa3c2dd0edefa630f55c9449d.jpg"/>
-               
+                <img className="header__logo" src="https://i.pinimg.com/originals/d0/6c/0a/d06c0a9aa3c2dd0edefa630f55c9449d.jpg" alt="site logo"/>
+
             </div>
 
             <div className="header__center">
                 <BookIcon/>
+                Recipes
                 <StorefrontIcon/>
-                
+                Shop
             </div>
 
             <div className="header__right">
-                This is text
+                Right
             </div>
         </div>
     )
