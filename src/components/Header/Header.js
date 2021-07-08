0cf045c86b60bc13b7b13ab36e57079f4967a805
Jeffrey Platt <jsplatt@gmail.com>
2021-07-08 17:08:50 -0400

    initial cleanup/header started

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img className="header__image" src="https://i.pinimg.com/originals/d0/6c/0a/d06c0a9aa3c2dd0edefa630f55c9449d.jpg"/>
+
+            </div>
+
+            <div className="header__center">
+                
+            </div>
+
+            <div className="header__right">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header
