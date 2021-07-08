bea5d78fc802fe795f98c47b782aea6532ffde6b
Jeffrey Platt <jsplatt@gmail.com>
2021-07-08 17:47:27 -0400

    working on design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import './Header.css'
+import BookIcon from '@material-ui/icons/Book';
+import StorefrontIcon from '@material-ui/icons/Storefront';
 
 function Header() {
     return (
         <div className="header">
             <div className="header__left">
-                <img className="header__image" src="https://i.pinimg.com/originals/d0/6c/0a/d06c0a9aa3c2dd0edefa630f55c9449d.jpg"/>
-
+                <img className="header__logo" src="https://i.pinimg.com/originals/d0/6c/0a/d06c0a9aa3c2dd0edefa630f55c9449d.jpg"/>
+               
             </div>
 
             <div className="header__center">
+                <BookIcon/>
+                <StorefrontIcon/>
                 
             </div>
 
             <div className="header__right">
-                
+                This is text
             </div>
         </div>
     )
