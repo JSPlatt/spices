a15bdbdc78612bda853643f8594ac7bebb47623d
Jeffrey Platt <jsplatt@gmail.com>
2021-07-09 12:12:20 -0400

    named app

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -5,9 +5,16 @@ function Body() {
     return (
         <div clasName="body">
             <div className="body__image">
-                <img src="https://www.allergicliving.com/wp-content/uploads/2014/07/94375228.jpg" alt="Spices"/>
-                <img src="https://www.rockrecipes.com/wp-content/uploads/2013/05/Smokin-Summer-Spice-Dry-Rub-close-up-image-of-spice-mix-in-a-mason-jar.jpg" alt="Spices"/>
+                <img 
+                    src="https://www.allergicliving.com/wp-content/uploads/2014/07/94375228.jpg" 
+                    alt="Spices"
+                />
+                <img 
+                    src="https://www.rockrecipes.com/wp-content/uploads/2013/05/Smokin-Summer-Spice-Dry-Rub-close-up-image-of-spice-mix-in-a-mason-jar.jpg" 
+                    alt="Spices"
+                />
             </div>
+            
             <div className="body__about">
                 <h1 style={{color: "burlywood"}}>ABOUT</h1>
                 <p>
