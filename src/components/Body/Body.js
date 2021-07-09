978aaf3863c193dc3df1593af79c40cd1841a7e1
Jeffrey Platt <jsplatt@gmail.com>
2021-07-09 11:10:58 -0400

    body started

---
+++ src/components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Body() {
+    return (
+        <div clasName="body">
+            <img src="https://www.allergicliving.com/wp-content/uploads/2014/07/94375228.jpg" className="body__image"/>
+            
+        </div>
+    )
+}
+
+export default Body
