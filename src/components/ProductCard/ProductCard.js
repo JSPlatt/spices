9a40c3373f5928cd608dec0dc373131ec059adce
Jeffrey Platt <jsplatt@gmail.com>
2021-07-12 12:54:38 -0400

    productCard started

---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ProductCard.css'
+
+function ProductCard() {
+    return (
+        <div>
+            <div className="product__card">
+                <img src="https://foodal.com/wp-content/uploads/2016/10/warming-spices-FB.jpg" alt="spices"/>
+            <p>description</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import ProductCard from '../ProductCard/ProductCard'
 import './Shop.css'
 
 function Shop() {
     return (
         <div>
-            <h1>This is the Shop Page</h1>
+           <h1>THIS IS THE SHOP PAGE 🚀 </h1>
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
         </div>
     )
 }
