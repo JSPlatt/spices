71a637aff3c9a7a7b1528b453834551e82f11794
Jeffrey Platt <jsplatt@gmail.com>
2021-07-13 10:40:24 -0400

    quick form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,17 @@ import './Contact.js'
 function Contact() {
     return (
         <div>
-            <h1>This is where contact info will go</h1>
+            <h1>CONTACT US</h1>
+            <br/>
+            <form>
+                <input placeholder="First Name..."/>
+                <input placeholder="Last Name..."/>
+                <input placeholder="E-mail..."/>
+                <input placeholder="Street Address..."/>
+                <input placeholder="City..."/>
+                <input placeholder="State..."/>
+                <input placeholder="Zip..."/>
+            </form>
         </div>
     )
 }
