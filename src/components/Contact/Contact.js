b89be39714a48af339a28f48731656e50e780cf8
Jeffrey Platt <jsplatt@gmail.com>
2021-07-13 12:19:01 -0400

    working on contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import './Contact.js'
+import './Contact.css'
 
 function Contact() {
     return (
         <div className="contact">
             <h1 style={{color: "burlywood"}}>CONTACT US</h1>
             <br/>
-            <form className="contact__form">
-                    <input placeholder="First Name..."/>
-                    <input placeholder="Last Name..."/>
-                    <input placeholder="E-mail..."/>
-                    <input placeholder="Street Address..."/>
-                    <input placeholder="City..."/>
-                    <input placeholder="State..."/>
-                    <input placeholder="Zip..."/>
-                <button style={{backgroundColor: "burlywood", fontWeight: "bolder"}}>SUBMIT</button>
+            <form className="contact__form" >
+                    <input style={{height: "25px", width: "300px", display:"box"}} placeholder="First Name..."/><br/>
+                    <input style={{height: "25px", width: "300px", display:"box"}} placeholder="Last Name..."/><br/>
+                    <input style={{height: "25px", width: "300px", display:"box"}} placeholder="E-mail..."/><br/>
+                    <input style={{height: "25px", width: "300px", display:"box"}} placeholder="Street Address..."/><br/>
+                    <input style={{height: "25px", width: "300px", display:"box"}} placeholder="City..."/><br/>
+                    <input style={{height: "25px", width: "300px", display:"box"}} placeholder="State..."/><br/>
+                    <input style={{height: "25px", width: "300px", display:"box"}} placeholder="Zip..."/><br/>
+                    <input placeholder="What's on your mind?" 
+                        style={{
+                            height: "60px",
+                            width: "250px"
+                        }}
+                    />
+                <button style={{backgroundColor: "burlywood",fontWeight: "bolder"}}>SUBMIT</button>
             </form>
         </div>
     )
