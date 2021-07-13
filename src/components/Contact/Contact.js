0e7ab3fb292ffbb7e216ffa544e1691f33356e13
Jeffrey Platt <jsplatt@gmail.com>
2021-07-13 11:19:43 -0400

    contemplation

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,17 +3,20 @@ import './Contact.js'
 
 function Contact() {
     return (
-        <div>
-            <h1>CONTACT US</h1>
+        <div className="contact">
+            <h1 style={{color: "burlywood"}}>CONTACT US</h1>
             <br/>
-            <form>
-                <input placeholder="First Name..."/>
-                <input placeholder="Last Name..."/>
-                <input placeholder="E-mail..."/>
-                <input placeholder="Street Address..."/>
-                <input placeholder="City..."/>
-                <input placeholder="State..."/>
-                <input placeholder="Zip..."/>
+            <form className="contact__form">
+                <div className="contact__input">
+                    <input placeholder="First Name..."/>
+                    <input placeholder="Last Name..."/>
+                    <input placeholder="E-mail..."/>
+                    <input placeholder="Street Address..."/>
+                    <input placeholder="City..."/>
+                    <input placeholder="State..."/>
+                    <input placeholder="Zip..."/>
+                </div>
+                <button style={{backgroundColor: "burlywood", fontWeight: "bolder"}}>SUBMIT</button>
             </form>
         </div>
     )
