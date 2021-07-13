ab46461b7c0c5b14659949b2949d738b91cdbf9a
Jeffrey Platt <jsplatt@gmail.com>
2021-07-13 11:28:01 -0400

    ...

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,6 @@ function Contact() {
             <h1 style={{color: "burlywood"}}>CONTACT US</h1>
             <br/>
             <form className="contact__form">
-                <div className="contact__input">
                     <input placeholder="First Name..."/>
                     <input placeholder="Last Name..."/>
                     <input placeholder="E-mail..."/>
@@ -15,7 +14,6 @@ function Contact() {
                     <input placeholder="City..."/>
                     <input placeholder="State..."/>
                     <input placeholder="Zip..."/>
-                </div>
                 <button style={{backgroundColor: "burlywood", fontWeight: "bolder"}}>SUBMIT</button>
             </form>
         </div>
