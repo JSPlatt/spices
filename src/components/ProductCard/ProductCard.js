fb5734f6f9e78c991127438c73655ad47268e69c
Jeffrey Platt <jsplatt@gmail.com>
2021-07-13 10:34:30 -0400

    basic cards done

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -7,6 +7,7 @@ function ProductCard() {
             <div className="product__card">
                 <img src="https://foodal.com/wp-content/uploads/2016/10/warming-spices-FB.jpg" alt="spices"/>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <button className="product__button">BUY</button>
             </div>
         </div>
     )
